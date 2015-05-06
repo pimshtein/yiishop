1fd1fd529ee42e57ec2f386a90238093cfa3edff
pimshtein <pimshtein@gmail.com>
2015-05-07 00:27:55 +0300

    change readme, add migration

---
+++ console/migrations/m150506_211331_create_brands_attr_attr_to_product.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150506_211331_create_brands_attr_attr_to_product extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150506_211331_create_brands_attr_attr_to_product cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
